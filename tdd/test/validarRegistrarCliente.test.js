f9ea9557816e7c77aee5add5c21e1ccea2418973
hspnet <166239409+hspnet@users.noreply.github.com>
2024-06-08 10:30:16 -0500

    Update validarRegistrarCliente.test.js
    se refactorizo las pruebas de validación.

--- tdd/test/validarRegistrarCliente.test.js
+++ tdd/test/validarRegistrarCliente.test.js
@@ -1,53 +1,27 @@
 /*
 Regsitar cliente
-campo usuario debe aceptar mínimo 5 caracteres y máximo 10 caracteres, no se permiten caracteres especiales ni espacios en blanco.
+campo nombre del cliente debe aceptar mínimo 5 caracteres y máximo 50 caracteres..
 campo contraseña debe tener como mínimo una letra mayúscula, una minúscula, un numero, un carácter especial  # $ % &, no se permiten espacios en blanco, y debe tener como mínimo 8 caracteres y máximo 15 caracteres.
-El campo email debe cumplir con los criterios mínimos, máximo 100 caracteres
+Valida que un email cumpla con los criterios mínimos de formato de correo electrónico y tenga minimo 10 caracteres y maximo 60 caracteres
 El campo dirección de aceptar como mínimo 10 caracteres y máximo 50 caracteres.
 */
 
-function validarUsuario(username) {
-    const regex = /^[a-zA-Z0-9]{5,10}$/;
-    return regex.test(username);
-}
 
-function validarPassword(password) {
-    const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[#\$%&])(?!.*\s).{8,15}$/;
-    return regex.test(password);
-}
+const {validarUsuario, validarPassword, validarNombreCliente, validarEmail, validarDireccion} = require("../src/utilsUser");
 
-function validarEmail(email) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email) && email.length >= 15 && email.length <= 60;
-}
+describe("validarNombreCliente", ()=> {
 
-function validarDireccion(address) {
-  return address.length >= 10 && address.length <= 50;
-}
-
-
-describe("validarUsuario", ()=> {
-
-    test('Valida nombres de usuario valido', () => {
-        expect(validarUsuario('userBien')).toBe(true);
-    });
-    
-    test('Valida nombres de usuario no tenga menos de 5 caracteres', () => {
-        expect(validarUsuario('user')).toBe(true);
-    });
-    
-    test('Valida nombres de usuario no tenga mas de 10 caracteres', () => {
-        expect(validarUsuario('userconmasdediez')).toBe(true);
-    });
-    
-    test('Valida nombres de usuario no tenga caracteres especiales ', () => {
-        expect(validarUsuario('user#123')).toBe(true);
-    });
-    
-    test('Valida nombres de usuario no tenga espacion en blanco ', () => {
-        expect(validarUsuario('user 123')).toBe(true);
+    test('Valida nombres del cliente correcto', () => {
+        expect(validarNombreCliente('Miguel Antonio Galvis')).toBe(true);
     });
 
+    test('Valida nombres del cliente con menos de 5 caracteres', () => {
+        expect(validarNombreCliente('Jose')).toBe(false);
+    });
+
+    test('Valida nombres del cliente con mas de 50 caracteres', () => {
+        expect(validarNombreCliente('Jose Miguel Antonio del Carmen Padilla Sotomayor Verenzuela')).toBe(false);
+    });
 });
 
 describe("validarPassword", ()=> {
@@ -58,31 +32,31 @@ describe("validarPassword", ()=> {
     });
     
     test('valida Contraseña sin mayuscula', () => {
-        expect(validarPassword('invalidpass1#')).toBe(true);
+        expect(validarPassword('invalidpass1#')).toBe(false);
     });
     
     test('Valida contraseña sin minuscula', () => {
-        expect(validarPassword('INVALID1#')).toBe(true);
+        expect(validarPassword('INVALID1#')).toBe(false);
     });
         
     test('valida contraseña sin numero', () => {
-        expect(validarPassword('NoNumber#')).toBe(true);
+        expect(validarPassword('NoNumber#')).toBe(false);
     });
     
     test('Valida constraseña sin caracter especial', () => {
-        expect(validarPassword('NoSpecial1')).toBe(true);
+        expect(validarPassword('NoSpecial1')).toBe(false);
     });
     
     test('Valida contraseña con espacio en blanco', () => {
-        expect(validarPassword('With space1#')).toBe(true);
+        expect(validarPassword('With space1#')).toBe(false);
     });
     
     test('valida contraseña con menos de 8 caracteres', () => {
-        expect(validarPassword('Short1#')).toBe(true);
+        expect(validarPassword('Short1#')).toBe(false);
     });
     
     test('valida contraseña con mas de 15 caracteres', () => {
-        expect(validarPassword('ThisIsAVeryLongPassword1#')).toBe(true);
+        expect(validarPassword('ThisIsAVeryLongPassword1#')).toBe(false);
     });
 
 });
@@ -93,12 +67,12 @@ describe("validarEmail", ()=> {
         expect(validarEmail('mi.email@example.com')).toBe(true);
     });
 
-    test('Valida correo con menos de 15 caracteres', () => {
-        expect(validarEmail('short@ex.co')).toBe(false);
+    test('Valida correo con menos de 10 caracteres', () => {
+        expect(validarEmail('s@ex.co')).toBe(false);
     });
     
     test('Valida correo con mas de 60 caracteres', () => {
-        expect(validarEmail('verylongemailaddressmorethansixtychars@example.com')).toBe(false);
+        expect(validarEmail('correoelectronicopersonaconmasdesesentacaracteres@example.com')).toBe(false);
     });
     
     test('Valida correo con espacios en blanco', () => {
