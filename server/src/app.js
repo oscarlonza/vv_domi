2805a3a82b275f16340314c09c23ceb4e33ccfcc
Oscar LonZa <fanfleto>
2024-06-09 11:23:31 -0500

    implemnetación de cors

--- server/src/app.js
+++ server/src/app.js
@@ -3,13 +3,28 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/authRoute.js';
 import productRouter from './routes/productRoute.js';
+import cors from 'cors';
+//import fileUpload from 'express-fileupload';
+
 
 const app = express();
 
 app.use(morgan('dev'));
+
+app.use(cors());
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+//app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+//app.use(express.static('public'));
+////FileUpload
+//app.use(fileUpload({
+//    useTempFiles: true,
+//    tempFileDir: '/tmp/',
+//    createParentPath: true
+//}));
+
 app.use('/api/auth', authRouter);
 app.use('/api/products', productRouter);
 
