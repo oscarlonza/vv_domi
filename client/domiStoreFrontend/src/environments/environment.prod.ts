80981f971de3cee8f2840409bf0cc0af7ef1fd1e
Oscar LonZa <fanfleto>
2024-06-15 08:12:55 -0500

    prod config file

--- client/domiStoreFrontend/src/environments/environment.prod.ts
+++ client/domiStoreFrontend/src/environments/environment.prod.ts
@@ -4,13 +4,3 @@ export const environment = {
     apiUrl: 'http://13.82.208.127:8080/api',
     accessToken: ''
 };
-lsarmiento@13.82.208.127
-Luz123456hel
-
-
-https://github.com/oscarlonza/oscarlonza.github.io.git
-
-rm -rf /usr/share/nginx/html/*
-
-sudo docker build -t domiserver -f Dockerfile.yml .
-sudo docker run -d -p 8080:8080 domiserver
