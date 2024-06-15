4898b053e2157aba6d3bb8630f0e5683f5839919
hspnet <hpeluffo@uninorte.edu.co>
2024-06-14 21:07:23 -0500

    Nuevas pruebas automatizadas

--- server/test/auth.test.js
+++ server/test/auth.test.js
@@ -10,20 +10,96 @@ import { addFileToRepository } from '../src/libs/fileManager.js';
  */
 describe('Pruebas de > Post api/auth/', () => {
 
-    /*it('Validar registrar usuario --> /login', async function () {
-
-        const req = { name:'Yunelis Gutierrez', email: 'ygutierrez@barranquilla.gov.co', address: 'Avenida siempre viva', password: 'Camilo1#.' };
+    it('Validar registrar usuario exitosamente--> /registrar', async function () {
+        const date = Date.now();
+        const req = { name: 'Yunelis Gutierrez', email: `hanspeluffodiaz+${date}@hotmail.com`, address: 'Avenida siempre viva', password: 'Camilo1#.' };
 
         const res = await request(app)
             .post('/api/auth/register')
             .send(req);
-        
+
         expect(res.status).to.equal(200);
-        //const { message } = JSON.parse(res.text);
-        expect(message).to.equal(req.name);
-        expect(message).to.equal(req.email);
-        expect(message).to.equal(req.address);
-    });*/
+
+    });
+
+    it('Validar registrar usuario duplicado --> /registrar', async function () {
+  
+        const req = { name: 'Yunelis Gutierrez', email: `hanspeluffodiaz@hotmail.com`, address: 'Avenida siempre viva', password: 'Camilo1#.' };
+
+        const res = await request(app)
+            .post('/api/auth/register')
+            .send(req);
+
+        expect(res.status).to.equal(404);
+        const { message } = JSON.parse(res.text);
+        expect(message).to.equal("El correo electrónico ya se encuentra registrado");
+    });
+
+    it('Validar registrar usuario, falta nombre--> /registrar', async function () {
+        const date = Date.now();
+        const req = { name: '', email: `hanspeluffodiaz+${date}@hotmail.com`, address: 'Avenida siempre viva', password: 'Camilo1#.' };
+
+        const res = await request(app)
+            .post('/api/auth/register')
+            .send(req);
+
+        expect(res.status).to.equal(500);
+        const { message } = JSON.parse(res.text);
+        expect(message).to.equal("name: Nombre requerido");
+    });
+
+    it('Validar registrar usuario, falta email--> /registrar', async function () {
+        const date = Date.now();
+        const req = { name: 'Juan Jose Ppolo', email: ``, address: 'Avenida siempre viva', password: 'Camilo1#.' };
+
+        const res = await request(app)
+            .post('/api/auth/register')
+            .send(req);
+
+        expect(res.status).to.equal(500);
+        const { message } = JSON.parse(res.text);
+        expect(message).to.equal("email: Correo electrónico requerido");
+    });
+
+    it('Validar registrar usuario, falta contraseña--> /registrar', async function () {
+        const date = Date.now();
+        const req = { name: 'Juan Jose Ppolo', email: `hanspeluffodiaz+${date}@hotmail.com`, address: 'Avenida siempre viva', password: '' };
+
+        const res = await request(app)
+            .post('/api/auth/register')
+            .send(req);
+
+        expect(res.status).to.equal(500);
+        const { message } = JSON.parse(res.text);
+        expect(message).to.equal("password: Contraseña requerida");
+    });
+
+    it('Validar registrar usuario, falta dirección--> /registrar', async function () {
+        const date = Date.now();
+        const req = { name: 'Juan Jose Ppolo', email: `hanspeluffodiaz+${date}@hotmail.com`, address: '', password: 'Hmmsdm55.1#' };
+
+        const res = await request(app)
+            .post('/api/auth/register')
+            .send(req);
+
+        expect(res.status).to.equal(500);
+        const { message } = JSON.parse(res.text);
+        expect(message).to.equal("address: Dirección requerida");
+    });
+
+    it('Validar registrar usuario, falta nombre, email, direccion y contraseña--> /registrar', async function () {
+        const date = Date.now();
+        const req = { name: '', email: ``, address: '', password: '' };
+
+        const res = await request(app)
+            .post('/api/auth/register')
+            .send(req);
+
+        expect(res.status).to.equal(500);
+        const { message } = JSON.parse(res.text);
+        expect(message).to.equal("name: Nombre requerido, email: Correo electrónico requerido, password: Contraseña requerida, address: Dirección requerida");
+    });
+
 
     it('Validar login correcto --> /login', async function () {
 
@@ -32,7 +108,7 @@ describe('Pruebas de > Post api/auth/', () => {
         const res = await request(app)
             .post('/api/auth/login')
             .send(req);
-        
+
         expect(res.status).to.equal(200);
     });
 
@@ -43,7 +119,7 @@ describe('Pruebas de > Post api/auth/', () => {
         const res = await request(app)
             .post('/api/auth/login')
             .send(req);
-        
+
         expect(res.status).to.equal(400);
         const { message } = JSON.parse(res.text);
         expect(message).to.equal("email: Correo requerido");
@@ -56,7 +132,7 @@ describe('Pruebas de > Post api/auth/', () => {
         const res = await request(app)
             .post('/api/auth/login')
             .send(req);
-        
+
         expect(res.status).to.equal(400);
         const { message } = JSON.parse(res.text);
         expect(message).to.equal("password: Contraseña requerida");
@@ -69,7 +145,7 @@ describe('Pruebas de > Post api/auth/', () => {
         const res = await request(app)
             .post('/api/auth/login')
             .send(req);
-        
+
         expect(res.status).to.equal(400);
         const { message } = JSON.parse(res.text);
         expect(message).to.equal("email: Correo requerido");
@@ -82,7 +158,7 @@ describe('Pruebas de > Post api/auth/', () => {
         const res = await request(app)
             .post('/api/auth/login')
             .send(req);
-        
+
         expect(res.status).to.equal(400);
         const { message } = JSON.parse(res.text);
         expect(message).to.equal("Correo o contraseña inválidos");
@@ -106,12 +182,12 @@ describe('Pruebas de > Post api/auth/', () => {
 
     it('Validar restablecer contraseña --> /resetpassword', async function () {
 
-        const req = { email: 'hpeluffo@uninorte.edu.co'};
+        const req = { email: 'hpeluffo@uninorte.edu.co' };
 
         const res = await request(app)
             .post('/api/auth/resetpassword')
             .send(req);
-        
+
         expect(res.status).to.equal(200);
         const { message } = JSON.parse(res.text);
         expect(message).to.equal("Será enviada una nueva contraseña al correo");
@@ -119,17 +195,120 @@ describe('Pruebas de > Post api/auth/', () => {
 
     /*it('Validar cambiar contraseña, contraseña incorrecta --> /changepassword', async function () {
 
-        const req = { oldPassword: 'Mipass987.#', password: 'Cambiar45.#'};
+        const req = { email: 'hanspeluffodiaz+1718408500022@hotmail.com', password: 'Camilo145#.' };
 
         const res = await request(app)
-            .post('/api/auth/changepassword')
+            .post('/api/auth/login')
             .send(req);
+
+        expect(res.status).to.equal(200);
+        //const { message } = JSON.parse(res.text);
+        //expect(message).to.equal("Correo o contraseña inválidos");
+
+        const reqCamPass= { oldPassword: 'Camilo145#.', password: 'Cambiar495.#'};
+
+        const resCambPass = await request(app)
+            .post('/api/auth/changepassword')
+            .send(reqCamPass);
+
+            console.log('respppp: ' + JSON.stringify(resCambPass))
         
-        expect(res.status).to.equal(400);
-        const { message } = JSON.parse(res.text);
-        expect(message).to.equal("Contraseña incorrecta");
+        expect(resCambPass.status).to.equal(200);
+        const { message } = JSON.parse(resCambPass.text);
+        expect(message).to.equal("Contraseña actualizada satisfactoriamente");
     });*/
 
-    
+    it('Validar registrar producto exitosamente --> /api/products', async function () {
 
+        const req = { email: 'admin@email.com', password: 'admin' };
+
+        const res = await request(app)
+            .post('/api/auth/login')
+            .send(req);
+
+        //console.log(`Response ${JSON.stringify(res)}`);
+        expect(res.status).to.equal(200);
+
+        const cookie = res.headers["set-cookie"];
+        const dataProducto = { name: "Nevera", description: "Nevera eficiente con refrigerador y congelador", price: 10000, quantity: 5, image: ""};
+        
+        //console.log(`Cookie >> ${cookie}`);
+
+        const resSaveProducto = await request(app)
+            .post('/api/products')
+            .set('Cookie', cookie)
+            .send(dataProducto);
+        
+        expect(resSaveProducto.status).to.equal(201);
+    });
+
+    it('Validar registrar producto sin permiso de admin --> /api/products', async function () {
+
+        const dataProducto = { name: "Nevera", description: "Nevera eficiente con refrigerador y congelador", price: 10000, quantity: 5, image: ""};
+
+        const resSaveProducto = await request(app)
+            .post('/api/products')
+            .send(dataProducto);
+        
+        expect(resSaveProducto.status).to.equal(401);
+        const { message } = JSON.parse(resSaveProducto.text);
+        expect(message).to.equal("No hay token, Autorización denegada");
+    });
+
+    it('Validar actualizar produto exitosamente --> /api/products', async function () {
+
+        const req = { email: 'admin@email.com', password: 'admin' };
+
+        const res = await request(app)
+            .post('/api/auth/login')
+            .send(req);
+
+        //console.log(`Response ${JSON.stringify(res)}`);
+        expect(res.status).to.equal(200);
+
+        const cookie = res.headers["set-cookie"];
+
+        const dataProducto = { name: "Nevera BBBB", description: "Nevera eficiente con refrigerador y congelador", price: 10000, quantity: 5};
+        
+        //console.log(`Cookie >> ${cookie}`);
+
+        const resUptProducto = await request(app)
+            .put('/api/products/66691131778cbb35bfaaefb7')
+            .set('Cookie', cookie)
+            .send(dataProducto);
+        //console.log(JSON.stringify(resUptProducto));
+
+            expect(resUptProducto.status).to.equal(200);
+            
+    });
+
+    it('Validar actualizar produto no existente --> /api/products', async function () {
+
+        const req = { email: 'admin@email.com', password: 'admin' };
+
+        const res = await request(app)
+            .post('/api/auth/login')
+            .send(req);
+
+        //console.log(`Response ${JSON.stringify(res)}`);
+        expect(res.status).to.equal(200);
+
+        const cookie = res.headers["set-cookie"];
+
+        const dataProducto = { name: "Nevera BBBB", description: "Nevera eficiente con refrigerador y congelador", price: 10000, quantity: 5};
+        
+        //console.log(`Cookie >> ${cookie}`);
+
+        const resUptProducto = await request(app)
+            .put('/api/products/66664351264bc70811f3298d')
+            .set('Cookie', cookie)
+            .send(dataProducto);
+        //console.log(JSON.stringify(resUptProducto));
+
+            expect(resUptProducto.status).to.equal(404);
+            const { message } = JSON.parse(resUptProducto.text);
+            expect(message).to.equal("Producto no encontrado");
+            
+    });
+    
 });
